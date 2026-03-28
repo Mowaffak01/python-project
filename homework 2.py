623678e14ae52989f5148c93eef1829c86b8b6a7
Mowaffak01 <mowaffakbarakat01@gmail.com>
2026-03-28 08:58:20 +0300

    Add files via upload

---
+++ homework 2.py
@@ -0,0 +1,33 @@
+num1 = 64
+num2 =7
+print("Number 1",num1)
+
+print("Number 2",num2)
+
+print("Addition :",num1+num2)
+
+print("Difference :",num1-num2)
+
+print("Product :",num1*num2)
+
+print("Divison :",num1/num2)
+
+print(" Floor Divison :",num1//num2)
+
+print("Modules Operation :",num1%num2)
+
+print("Square :",num2*num2)
+
+print("Square Root :",num2**3.5)
+
+print("Equal ? :",num1==num2)
+
+print("Number 1 greater ? :",num1>num2)
+
+print("Number 2 greater ? :",num1<num2)
+
+print("Not Equal ? :",num1!=num2)
+
+result = num1/7+num2**7+54
+print("Result of Given Equation is : ",result)
+
