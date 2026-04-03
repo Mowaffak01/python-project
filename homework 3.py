2950c68f42a8ef76918e7f0cec745df5711dbb27
Mowaffak01 <mowaffakbarakat01@gmail.com>
2026-04-03 09:47:54 +0300

    Add files via upload

---
+++ homework 3.py
@@ -0,0 +1,10 @@
+num = int(input("enter number to check your average in exams:"))
+
+if num>90:
+    print("outstading!")
+    if num%2==0 :
+        print("congrats a certficte of exellenct is at your mail")
+    else :
+          
+        print("Average")
+        print(" You can do better try harder next time! ")
