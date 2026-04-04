20619be55c0c5cce78e173da6db4f42a457f9248
Mowaffak01 <mowaffakbarakat01@gmail.com>
2026-04-04 10:45:40 +0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,15 +1,20 @@
-def recur_factorial(n):
-    if n==1:
-        return n
-    else:
-        return n*recur_factorial (n-1)
-num = int(input("enter a number"))
-if num<0:
-    print("srorry factural number does not exist for negative numbers")
-    
-elif num ==0:
-    
-    print("the factural number is 0 or 1")
-    
-else:
-    print("the factural number of",num"is"recur_factorial(num))
+def add(x,y):
+  return x + y
+
+def subtract(x,y):
+  return x - y
+
+def multiply(x,y):
+  return x * y
+
+def divide(x,y):
+  return x / y
+
+num1=int (input("enter number 1"))
+num2=int (input("enter number 2"))
+
+print("Sum :",add(num1,num2))
+
+print("Difference :",subtract(num1,num2))
+print("Product :",multiply(num1,num2))
+print("Qutiont :",divide(num1,num2))
