ecd898e9040b3155cc5177063373336a8907928b
Mowaffak01 <mowaffakbarakat01@gmail.com>
2026-04-04 10:30:25 +0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,6 +1,15 @@
-def intro(name):
+def recur_factorial(n):
+    if n==1:
+        return n
+    else:
+        return n*recur_factorial (n-1)
+num = int(input("enter a number"))
+if num<0:
+    print("srorry factural number does not exist for negative numbers")
     
-    print("hello good moring i am",name)
+elif num ==0:
     
-    user_name = input("enter your name")
-    intro(user_name)
+    print("the factural number is 0 or 1")
+    
+else:
+    print("the factural number of",num"is"recur_factorial(num))
