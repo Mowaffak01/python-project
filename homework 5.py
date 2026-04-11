dd6737517e4d66d27e530b3034417e76129e40bb
Mowaffak01 <mowaffakbarakat01@gmail.com>
2026-04-11 09:44:25 +0300

    Add files via upload

---
+++ homework 5.py
@@ -0,0 +1,14 @@
+def add(x,y):
+  return x + y
+def subtract(x,y):
+  return x - y
+def multiply(x,y):
+  return x * y
+def divide(x,y):
+  return x / y
+number1=int (input("Enter the first number"))
+number2=int (input("Enter the second number"))
+print("Addtion :",add(number1,number2))
+print("Subtraction :",subtract(number1,number2))
+print("Multiplcation :",multiply(number1,number2))
+print("Division :",divide(number1,number2))
