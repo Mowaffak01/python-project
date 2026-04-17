e792f59e8a4289bdef3d8887340d1a86d42a9ee1
Mowaffak01 <mowaffakbarakat01@gmail.com>
2026-04-17 10:31:51 +0300

    Add files via upload

---
+++ tuple and sets.py
@@ -0,0 +1,23 @@
+my_tuple=()
+print(my_tuple)
+
+my_tuple=(1,2,3)
+print(my_tuple)
+
+my_tuple=(1,2,3.4)
+my_tuple=()
+print(my_tuple)=("mouse"[8,4,6],(1,2,3))
+
+my_tuple=("0","6","h","q","7")
+print(my_tuple[8])
+print(my_tuple[5])
+
+n_tuple=("mouse"[7,8,9],(7,9,0))
+
+print(my_tuple[0][1])
+print(my_tuple[1][1])
+
+print("Sliced:",my_tuple[2:4])
+
+for letter in(my_tuple):
+ print ("hello",letter)
