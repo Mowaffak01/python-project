c4105e906847074df5d58d0a3caa9799d8a55344
Mowaffak01 <mowaffakbarakat01@gmail.com>
2026-04-18 09:46:18 +0300

    Add files via upload

---
+++ homework 8.py
@@ -0,0 +1,34 @@
+
+my_tuple = ()
+print(my_tuple)
+
+
+my_tuple = (3, 6, 9)
+print(my_tuple)
+
+
+my_tuple = (1, "Hi whoever is seeing this", 3.4)
+print(my_tuple)
+
+
+my_tuple = ("Is my code good", [9, 6, 3], (8, 1, 5))
+print(my_tuple)
+
+
+my_tuple = ('u','a','r','e','i','t')
+print(my_tuple[1])   
+print(my_tuple[4])   
+
+
+n_tuple = ("Is my code good", [2, 9, 1], (7, 8, 3))
+
+
+print(n_tuple[0][7])       
+print(n_tuple[4][9])      
+
+
+print("Sliced this tuple :", my_tuple[3:6])
+
+
+for letter in (my_tuple):
+    print("Hi mrs shena.k", letter)
