9b0dba96723cf6ab7feb5a1e198c7f8081706f31
Mowaffak01 <mowaffakbarakat01@gmail.com>
2026-04-24 10:18:28 +0300

    Add files via upload

---
+++ conctructer.py
@@ -0,0 +1,15 @@
+class enploye:
+    
+   def __init__(self):
+ 
+    print("enploye called.")
+
+   def __del__(self):
+     print("destroctor called")
+     
+     
+     
+obj= enploye
+del obj
+
+
