05ece1922dfcac011182ca6ab1d583c134bb3152
Mowaffak01 <mowaffakbarakat01@gmail.com>
2026-05-02 08:42:22 +0300

    Add files via upload

--- homework 8.py
+++ homework 8.py
@@ -1,34 +1,31 @@
+from abc import ABC,abstractmethod
 
-my_tuple = ()
-print(my_tuple)
+class insecets(ABC):
+    def move(self):
+        pass
+    
+class spider(insecets):
+        def print(self):
+            print("i have 8 legs")
+            
+class ant(insecets):
+        def print(self):
+            print("i have 2 antenas")
+class centipeds(insecets):
+        def print(self):
+            print("i have 100 legs")
+class millipeds(insecets):
+        def print(self):
+            print("i have 1000 legs")
+            
+R = spider
+R.move()
 
+k = ant
+k.move()
 
-my_tuple = (3, 6, 9)
-print(my_tuple)
+R = centipeds
+R.move()
 
-
-my_tuple = (1, "Hi whoever is seeing this", 3.4)
-print(my_tuple)
-
-
-my_tuple = ("Is my code good", [9, 6, 3], (8, 1, 5))
-print(my_tuple)
-
-
-my_tuple = ('u','a','r','e','i','t')
-print(my_tuple[1])   
-print(my_tuple[4])   
-
-
-n_tuple = ("Is my code good", [2, 9, 1], (7, 8, 3))
-
-
-print(n_tuple[0][7])       
-print(n_tuple[4][9])      
-
-
-print("Sliced this tuple :", my_tuple[3:6])
-
-
-for letter in (my_tuple):
-    print("Hi mrs shena.k", letter)
+k = millipeds
+k.move()
