96f7a05fab226f7b8070b46fabfb156c0748af0f
Mowaffak01 <mowaffakbarakat01@gmail.com>
2026-06-01 15:03:02 +0300

    Add files via upload

--- operations on a file.py
+++ operations on a file.py
@@ -1,14 +1,16 @@
-file = open('Codingal.txt','r')
-print(file.read())
-file.close
+file_read = open('Codingal.txt','r')
+print("file in read mode")
+print(file_read.read())
+file_read.close
 
 
-file = open('Codingal.txt','r')
-print("\n in read parts \n")
-print(file.read(8))
-file.close()
+file_write = open('Codingal.txt','w')
+file_write.write("file in write mode")
+file_write.write("Hi i am a penguin andd i am 1yr old")
+file_write.close()
 
 
-file = open('Codingal.txt','a')
-print("Hi i am a penguin andd i am 1yr old")
-file.close()
+file_append = open('Codingal.txt','a')
+file_append.write("file in append mode")
+file_append.write("Hi i am a penguin andd i am 1yr old")
+file_append.close()
