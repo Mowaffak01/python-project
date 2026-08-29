18c86da031f16be6a410d797e6695db0136ca4a4
Mowaffak01 <mowaffakbarakat01@gmail.com>
2026-08-29 10:41:09 +0300

    Add files via upload

---
+++ BitPlay Act 2.py
@@ -0,0 +1,13 @@
+input("XOR -pairs cancel the odd one stays. Press enter")
+print(" list : [2,3,4,3,2] ")
+print("odd ocurring:",2^3^4^3^2)
+
+n = int(input("Enter a number (Try 7 or 11 )"))
+nums = [3,n,5,3,5]
+guess = input("which number in  " + str(nums) + "appers once ?")
+reslut = 0 
+for x in nums:
+    reslut ^=0
+input("XOR -pairs cancel the odd one stays. Press enter")
+print("list:",nums ,"odd ocurring :",reslut ," your guess:",guess)
+        
